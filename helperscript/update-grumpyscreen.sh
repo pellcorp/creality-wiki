f0627840d67efb873505e7d834d438251d590bba
Jason Pell <jason@pellcorp.com>
2025-07-11 09:24:45 +1000

    ensure that update guppyscreen resets the config

--- helperscript/update-grumpyscreen.sh
+++ helperscript/update-grumpyscreen.sh
@@ -18,4 +18,20 @@ else
     echo "ERROR: Grumpyscreen could not be downloaded!"
     exit 0
 fi
+
+jq '.default_macros.cooldown="SET_HEATER_TEMPERATURE HEATER=extruder TARGET=0"' /usr/data/guppyscreen/guppyscreen.json > /usr/data/guppyscreen.json.$$
+mv /usr/data/guppyscreen.json.$$ /usr/data/guppyscreen/guppyscreen.json
+
+jq '.default_macros.load_filament="_GUPPY_LOAD_MATERIAL EXTRUDER_TEMP={}"' /usr/data/guppyscreen/guppyscreen.json > /usr/data/guppyscreen.json.$$
+mv /usr/data/guppyscreen.json.$$ /usr/data/guppyscreen/guppyscreen.json
+
+jq '.default_macros.unload_filament="_GUPPY_QUIT_MATERIAL EXTRUDER_TEMP={}"' /usr/data/guppyscreen/guppyscreen.json > /usr/data/guppyscreen.json.$$
+mv /usr/data/guppyscreen.json.$$ /usr/data/guppyscreen/guppyscreen.json
+
+jq '.guppy_restart_cmd="/etc/init.d/S99guppyscreen restart"' /usr/data/guppyscreen/guppyscreen.json > /usr/data/guppyscreen.json.$$
+mv /usr/data/guppyscreen.json.$$ /usr/data/guppyscreen/guppyscreen.json
+
+jq '.guppy_update_cmd="/usr/data/printer_data/config/GuppyScreen/update-grumpyscreen.sh"' /usr/data/guppyscreen/guppyscreen.json > /usr/data/guppyscreen.json.$$
+mv /usr/data/guppyscreen.json.$$ /usr/data/guppyscreen/guppyscreen.json
+
 /etc/init.d/S99guppyscreen restart
